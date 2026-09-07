491f0205ae02c92b3e0950aa1d99e1ba2fc34fc3
Yaw Boakye at Zebo <yaw@zebo.dev>
2026-09-07 11:05:51 +0100

    test: disable external script loading

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    environmentOptions: {
+      happyDOM: {
+        settings: {
+          disableJavaScriptFileLoading: true,
+        },
+      },
+    },
     include: ['packages/**/*.test.ts'],
     restoreMocks: true,
   },
